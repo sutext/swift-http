bf8b4cf51aaf79fa8aacb54cee199f5e584df7cc
supertext <supertext@icloud.com>
2025-04-15 18:24:25 +0800

    Formatting dependency

--- Package.swift
+++ Package.swift
@@ -7,19 +7,15 @@ let package = Package(
     name: "swift-http",
     platforms: [.iOS(.v13),.watchOS(.v6),.macOS(.v10_15),.tvOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "HTTP",
             targets: ["HTTP"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/sutext/swift-json", from: "2.4.0"),
         .package(url: "https://github.com/sutext/swift-promise", from: "2.0.2")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HTTP",
             dependencies: [
