cb556e20365145b6ea7815e3ad18d3d11cc782b1
supertext <supertext@icloud.com>
2025-04-17 15:07:52 +0800

    upgradge Promise

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/sutext/swift-json", from: "2.4.1"),
-        .package(url: "https://github.com/sutext/swift-promise", from: "2.0.2")
+        .package(url: "https://github.com/sutext/swift-promise", from: "2.1.0")
     ],
     targets: [
         .target(
